ccbf5699bc99e776aacc72bed3a3cca02010c866
sadey <sadeychai@outlook.com>
2017-08-10 15:45:26 +0800

    first commit

---
+++ src/main/java/com/sadey/tapestry/pages/Home.java
@@ -0,0 +1,12 @@
+package com.sadey.tapestry.pages;
+
+import org.apache.tapestry.html.BasePage;
+
+/**
+ * Created by chaihaipeng on 2017/8/8.
+ */
+public class Home extends BasePage {
+    public String getGreetingSubject() {
+        return "ddd";
+    }
+}
